5f99113fd9c57639dffac77ec890784a25b2ffa7
Roberto Rodrigues <111910388+BetoPRodrigues@users.noreply.github.com>
2024-03-01 18:54:42 +0000

    exercicios varaiaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,17 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Roberto'
+sobrenome = "Pawelski"
+texto = '''Eu,
+escrevi em
+3 linhas'''
 
 # Declare variáveis de cada tipo numérico
-
+mesesAno = 12
+valorDecimal = 1234.00
+somatorio = 1+2j
 
 # Declare uma variável do tipo booleano
+ativo = True
+inativo = False
